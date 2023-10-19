48f1e760ef381094915eaaca82c7ef7fe840b5cf
DonatoBastione <dbastione23@fed.idserve.net>
2023-10-19 10:14:12 +0200

    Initial Commit

---
+++ CollApp/CollAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollAppApp.swift
+//  CollApp
+//
+//  Created by DONATO BASTIONE on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CollAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
