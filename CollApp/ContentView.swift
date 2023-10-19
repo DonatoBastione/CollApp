b2798b9f355b63b3fb4bb2e6d32f3155a8a657b6
adrpr97 <130666088+adrpr97@users.noreply.github.com>
2023-10-19 11:02:42 +0200

    test3
    test3

--- CollApp/ContentView.swift
+++ CollApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, fedora I'm giuseppe'!")
+            Text("Hello, fedora I'm giuseppe! i'm adriano")
         }
         .padding()
     }
