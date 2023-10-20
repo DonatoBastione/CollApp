47104ab6d402965c80afd2bfedbc1f63c8966e40
adrpr97 <130666088+adrpr97@users.noreply.github.com>
2023-10-20 12:50:20 +0200

    schermata dei due bottoni
    schermata due bottoni viola, da cambiare solo il font nei bottoni, ADRIANO

--- CollApp/View/ContentView.swift
+++ CollApp/View/ContentView.swift
@@ -9,15 +9,95 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello Fedora!")
+            Text("Choose the modality ")
+           //.font(
+               //.system(size: 24, weight: .semibold, design: .default)
+                 
+             //Font.custom("SF Pro", size: 24, relativeTo: .title)
+               
+                
+                .font(
+                    Font.custom("Times New Roman", size: 24)
+                        .weight(.black)
+                )
+            
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.black)
+                
+        }
+        .padding(.top,-100)
+        
+        VStack{
+            ZStack{
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 233, height: 73)
+                    .background(Color(red: 0.68, green: 0.22, blue: 0.72))
+                    .cornerRadius(20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .inset(by: 2)
+                            .stroke(Color(red: 0.64, green: 0.22, blue: 0.68), lineWidth: 4)
+                    )
+                VStack{
+                    HStack{
+                        Text("Single")
+                        Image(systemName: "person.fill")
+                    }
+                    .font(
+                        Font.custom("SF Pro", size: 28)
+                            .weight(.semibold)
+                    )
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                    .frame(width: 181, height: 27, alignment: .top)
+                    
+                    
+                }
+            }
         }
         .padding()
+        VStack{
+            ZStack{
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 233, height: 73)
+                    .background(Color(red: 0.68, green: 0.22, blue: 0.72))
+                    .cornerRadius(20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .inset(by: 2)
+                            .stroke(Color(red: 0.64, green: 0.22, blue: 0.68), lineWidth: 4)
+                    )
+                VStack{
+                    HStack{
+                        Text("Team")
+                        Image(systemName: "person.3.fill")
+                    }
+                    .font(
+                        Font.custom("SF Pro", size: 28)
+                            .weight(.semibold)
+                    )
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                    .frame(width: 181, height: 27, alignment: .top)
+                    
+                    
+                }
+            }
+        }
+        
+        
+        
     }
 }
+
+
+
+
 
 #Preview {
     ContentView()
