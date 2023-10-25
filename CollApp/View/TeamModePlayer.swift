0b4e81e327b4fd1b13a0ac67c34fe35dfc14d66a
Giuseppe <golivari1@gmail.com>
2023-10-25 09:58:38 +0200

    teammodeplayer

---
+++ CollApp/View/TeamModePlayer.swift
@@ -0,0 +1,18 @@
+//
+//  TeamModePlayer.swift
+//  CollApp
+//
+//  Created by Giuseppe Olivari on 25/10/23.
+//
+
+import SwiftUI
+
+struct TeamModePlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeamModePlayer()
+}
