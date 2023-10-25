62a7b212830121c014c5a0ef37bf054ceb201724
Giuseppe <golivari1@gmail.com>
2023-10-25 09:43:12 +0200

    ahhhhhhh

---
+++ CollApp/View/SModeAlt.swift
@@ -0,0 +1,18 @@
+//
+//  SModeAlt.swift
+//  CollApp
+//
+//  Created by Giuseppe Olivari on 25/10/23.
+//
+
+import SwiftUI
+
+struct SModeAlt: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SModeAlt()
+}
