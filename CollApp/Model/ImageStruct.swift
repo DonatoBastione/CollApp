2bbfc8fd0b120eb8430cf91d7262c1ffc6b92f25
adrpr97 <130666088+adrpr97@users.noreply.github.com>
2023-10-26 11:34:08 +0200

    single mode alt

---
+++ CollApp/Model/ImageStruct.swift
@@ -0,0 +1,16 @@
+//
+//  ImageStruct.swift
+//  CollApp
+//
+//  Created by Adriano Prota on 25/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct ImageStruct: Identifiable{
+    var id = UUID()
+    
+    var imageAvatar: String
+    
+}
---
+++ CollApp/ViewModel/ImageClass.swift
@@ -0,0 +1,26 @@
+//
+//  ImageClass.swift
+//  CollApp
+//
+//  Created by Adriano Prota on 25/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+class ImageClass{
+    
+    var pupazzetti = [
+    ImageStruct(imageAvatar: "noimage"),
+    ImageStruct(imageAvatar: "avatar1"),
+    ImageStruct(imageAvatar: "avatar2"),
+    ImageStruct(imageAvatar: "avatar3"),
+    ImageStruct(imageAvatar: "avatar4"),
+    ImageStruct(imageAvatar: "avatar5"),
+    ImageStruct(imageAvatar: "avatar6"),
+    ImageStruct(imageAvatar: "avatar7"),
+    ImageStruct(imageAvatar: "avatar8"),
+    ImageStruct(imageAvatar: "avatar9")
+    ]
+}
