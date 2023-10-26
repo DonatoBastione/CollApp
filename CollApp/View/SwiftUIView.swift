55410f24781aee6e7efac7de7c0c53c3d6265b83
Giulsd07 <dimeo.giulia.98@gmail.com>
2023-10-26 11:22:57 +0200

    Create SwiftUIView.swift

---
+++ CollApp/View/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  CollApp
+//
+//  Created by Giulia Di Meo on 26/10/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
