a6c85c81a4e8349698bf97c4fbc334dd16bccd84
Giuseppe <golivari1@gmail.com>
2023-10-27 11:26:37 +0200

    done botton

--- CollApp/View/FinalView.swift
+++ CollApp/View/FinalView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct FinalView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            NavigationLink(destination: ContentView()) {
+                Text("Done") .padding(.leading, 250.0)
+            }
+                
+            
+            
+            
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
-
 #Preview {
     FinalView()
 }
