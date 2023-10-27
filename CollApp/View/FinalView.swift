bbcf9c3f6af53a07f3e7c4f5e69fed116393d50e
Giulsd07 <dimeo.giulia.98@gmail.com>
2023-10-27 12:57:01 +0200

    aaaa

--- CollApp/View/FinalView.swift
+++ CollApp/View/FinalView.swift
@@ -9,78 +9,89 @@ import SwiftUI
 
 struct FinalView: View {
     var body: some View {
-        NavigationStack {
-            NavigationLink(destination: ContentView()) {
-                Text("Done")
-                    .padding(.leading,280)
-            }
-
-            VStack{
-                //Image(systemName: "fireworks")
-                Text("THE CHALLANGE IS \n        COMPLETED")
-                    .padding(.top,UIScreen.main.bounds.width/7)
-                    .padding(.bottom,50)
-                    .bold()
-                    .font(.title)
+        
+            NavigationStack {
                 ZStack{
-                    Image(systemName: "fireworks")
+                    
+                    Image("BackGround")
                         .resizable()
+                        .ignoresSafeArea()
+                        .scaledToFill()
+                NavigationLink(destination: ContentView()) {
+                    Text("Done")
+                        .padding(.leading,280)
+                        .padding(.bottom,700)
+                  
+                }
+                
+                VStack{
+                    //Image(systemName: "fireworks")
+                    Text("THE CHALLANGE IS \n        COMPLETED")
+                        .padding(.top,UIScreen.main.bounds.width/7)
+                        .padding(.bottom,50)
+                        .bold()
+                        .font(.title)
+                    ZStack{
+                        Image(systemName: "fireworks")
+                            .resizable()
                         
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70)
-                        .padding(.trailing,200)
-                        .padding(.bottom,330)
-                    
-                    
-                    Image(systemName: "fireworks")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70)
-                        .padding(.bottom,450)
-                        .padding(.trailing,130)
-                    
-                    
-                    Image(systemName: "fireworks")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70)
-                        .padding(.bottom,450)
-                        .padding(.trailing,280)
-                    
-                    
-                    
-                    Text("Bartolomeo\nYou didn’t completed the task\nDrive your friends to go shopping")
-                        .frame(width: 250, height: 150)
-                        .border(Color.accentColor)
-                    
-                    
-                    
-                        .padding(.bottom,100)
-                    
-                    Image(systemName: "fireworks")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70)
-                        .padding(.leading,200)
-                        .padding(.top,280)
-                    
-                    
-                    Image(systemName: "fireworks")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70)
-                        .padding(.leading,250)
-                        .padding(.top,140)
-                    
-                    Image(systemName: "fireworks")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70)
-                        .padding(.top,140)
-                        .padding(.leading,120)
-                    
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 70)
+                            .padding(.trailing,200)
+                            .padding(.bottom,330)
+                        
+                        
+                        Image(systemName: "fireworks")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 70)
+                            .padding(.bottom,450)
+                            .padding(.trailing,130)
+                        
+                        
+                        Image(systemName: "fireworks")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 70)
+                            .padding(.bottom,450)
+                            .padding(.trailing,280)
+                        
+                        
+                        
+                        Text("Bartolomeo\nYou didn’t completed the task\nDrive your friends to go shopping")
+                            .frame(width: 250, height: 150)
+                            .border(Color.accentColor)
+                        
+                        
+                        
+                            .padding(.bottom,100)
+                        
+                        Image(systemName: "fireworks")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 70)
+                            .padding(.leading,200)
+                            .padding(.top,280)
+                        
+                        
+                        Image(systemName: "fireworks")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 70)
+                            .padding(.leading,250)
+                            .padding(.top,140)
+                        
+                        Image(systemName: "fireworks")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 70)
+                            .padding(.top,140)
+                            .padding(.leading,120)
+                        
+                    }
                 }
             }
+            
         }
     }
 }
