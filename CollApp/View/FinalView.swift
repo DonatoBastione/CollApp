13f49187b68539cd7b913493799f997098967b9f
Giulsd07 <dimeo.giulia.98@gmail.com>
2023-10-27 12:16:50 +0200

    finalview

--- CollApp/View/FinalView.swift
+++ CollApp/View/FinalView.swift
@@ -9,7 +9,69 @@ import SwiftUI
 
 struct FinalView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            //Image(systemName: "fireworks")
+            Text("THE CHALLANGE IS \n        COMPLETED")
+                .padding(.top,UIScreen.main.bounds.width/5)
+                .padding(.bottom,50)
+                .bold()
+                .font(.title)
+            ZStack{
+                Image(systemName: "fireworks")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 70)
+                    .padding(.trailing,200)
+                    .padding(.bottom,330)
+                
+                
+                Image(systemName: "fireworks")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 70)
+                    .padding(.bottom,450)
+                    .padding(.trailing,130)
+                    
+                
+                Image(systemName: "fireworks")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 70)
+                    .padding(.bottom,450)
+                    .padding(.trailing,280)
+                
+                
+                
+                Text("Bartolomeo\nYou didn’t completed the task\nDrive your friends to go shopping")
+                    .frame(width: 250, height: 150)
+                    .border(Color.accentColor)
+                    
+                    .padding(.bottom,100)
+               
+                Image(systemName: "fireworks")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 70)
+                    .padding(.leading,200)
+                    .padding(.top,280)
+                
+                
+                Image(systemName: "fireworks")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 70)
+                    .padding(.leading,250)
+                    .padding(.top,140)
+                
+                Image(systemName: "fireworks")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 70)
+                    .padding(.top,140)
+                    .padding(.leading,120)
+                
+            }
+        }
     }
 }
 
