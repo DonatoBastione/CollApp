931894104f1df5ae858ad27222a1a96ab913fddc
Giuseppe <golivari1@gmail.com>
2023-10-27 11:37:32 +0200

    hy5gt4rfe

--- CollApp/View/FinalView.swift
+++ CollApp/View/FinalView.swift
@@ -10,15 +10,19 @@ import SwiftUI
 struct FinalView: View {
     var body: some View {
         NavigationStack{
-            NavigationLink(destination: ContentView()) {
-                Text("Done") .padding(.leading, 250.0)
-            }
+            
+           
+                NavigationLink(destination: ContentView()) {
+                    Text("Done") .padding(.leading, 250.0)
+                    
+                }
                 
-            
-            
-            
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        }
+                
+                
+                
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+        
     }
 }
 #Preview {
