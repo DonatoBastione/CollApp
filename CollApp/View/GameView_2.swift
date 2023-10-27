8f6d6ffe56697f5ff85e85fb156b207c28ad1e2b
Giuseppe <golivari1@gmail.com>
2023-10-27 12:36:05 +0200

    fix

--- CollApp/View/GameView_2.swift
+++ CollApp/View/GameView_2.swift
@@ -9,37 +9,43 @@ import SwiftUI
 
 struct GameView_2: View {
     var body: some View {
-        ZStack{
-            Image("BackGround")
-                .resizable()
-                .ignoresSafeArea()
-                .opacity(0.5)
-                .scaledToFill()
+        NavigationStack {
             ZStack{
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Image ("Ellipse 4")
-                        .resizable()
-                        .frame(width: 110, height: 110)
-                        .padding(.top,UIScreen.main.bounds.height/60)
-                        .padding(.bottom,UIScreen.main.bounds.width/60)
-                        .padding(.leading,UIScreen.main.bounds.width/60)
-                        .padding(.trailing,UIScreen.main.bounds.width/60)
-                    
-                    
-                })
-                Text("Stop")
-                    .font(.title)
-                    .bold()
-                    .foregroundColor(.white)
-                    .padding(.top,UIScreen.main.bounds.height/60)
-                    .padding(.bottom,UIScreen.main.bounds.width/60)
-                    .padding(.leading,UIScreen.main.bounds.width/60)
-                    .padding(.trailing,UIScreen.main.bounds.width/60)            }
-    
-            
-            
-                   HStack{
-    
+                
+                Image("BackGround")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .opacity(0.5)
+                    .scaledToFill()
+                
+                ZStack{
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        
+                        NavigationLink(destination: FinalView()) {
+                            Image ("Ellipse 4")
+                                .resizable()
+                                .frame(width: 110, height: 110)
+                                .padding(.top,UIScreen.main.bounds.height/60)
+                                .padding(.bottom,UIScreen.main.bounds.width/60)
+                                .padding(.leading,UIScreen.main.bounds.width/60)
+                                .padding(.trailing,UIScreen.main.bounds.width/60)
+                            
+                        }
+                    })
+                    NavigationLink(destination: FinalView()){
+                        Text("Stop")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(.top,UIScreen.main.bounds.height/60)
+                            .padding(.bottom,UIScreen.main.bounds.width/60)
+                            .padding(.leading,UIScreen.main.bounds.width/60)
+                        .padding(.trailing,UIScreen.main.bounds.width/60)            }
+                }
+                
+                
+                
+                
                 VStack {
                     VStack{
                         Image("avatar1")
@@ -61,19 +67,19 @@ struct GameView_2: View {
                                 }
                             })
                             
-                    }
+                        }
                     }.frame(width:UIScreen.main.bounds.width/2.2,height: UIScreen.main.bounds.height/4)
                         .position(x:UIScreen.main.bounds.width/2,y:UIScreen.main.bounds.height/5)
-                        
-                        
+                    
+                    
                     
                     
                     VStack{
                         Image("avatar5")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            
-                            
+                        
+                        
                         Text ("Player3")
                         ZStack{
                             
@@ -91,31 +97,32 @@ struct GameView_2: View {
                                     }
                                 })
                                 
-                        }
-                                
+                            }
+                            
                             
                         }
-                    
+                        
                     }.frame(width:UIScreen.main.bounds.width/2.2,height: UIScreen.main.bounds.height/4)
                     .position(x:UIScreen.main.bounds.width/2,y:UIScreen.main.bounds.height/4.5)}
                 
-                       
                 
                 
+                
+                
+                
+                
+                
+                
+                
+            }.background(Image("GameView2"))
+                .toolbar(.hidden)
             
-            }
             
-           
-            
-            
-        }.background(Image("GameView2"))
-            .toolbar(.hidden)
-       
-            
-       
+        }
     }
 }
 
 #Preview {
     GameView_2()
 }
+
--- CollApp/View/GameView_3.swift
+++ CollApp/View/GameView_3.swift
@@ -9,78 +9,51 @@ import SwiftUI
 
 struct GameView_3: View {
     var body: some View {
-        
-        ZStack{
-            Image("BackGround")
+        NavigationStack {
+            ZStack{
+Image("BackGround")
                 .resizable()
                 .ignoresSafeArea()
                 .opacity(0.5)
                 .scaledToFill()
-            ZStack{
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Image ("Ellipse 4")
-                        .resizable()
-                        .frame(width: 110, height: 110)
-                        .padding(.top,UIScreen.main.bounds.height/60)
-                        .padding(.bottom,UIScreen.main.bounds.width/60)
-                        .padding(.leading,UIScreen.main.bounds.width/60)
-                        .padding(.trailing,UIScreen.main.bounds.width/60)
-                    
-                    
-                })
-                Text("Stop")
-                    .font(.title)
-                    .bold()
-                    .foregroundColor(.white)
-                    .padding(.top,UIScreen.main.bounds.height/60)
-                    .padding(.bottom,UIScreen.main.bounds.width/60)
-                    .padding(.leading,UIScreen.main.bounds.width/60)
-                    .padding(.trailing,UIScreen.main.bounds.width/60)            }
-    
-            
-            
-                   HStack{
-    
-                VStack {
-                    VStack{
-                        Image("avatar1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                        
-                        Text ("Player1")
-                        ZStack{
-                            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                                ZStack{
-                                    Image("Ellipse 23")
-                                        .resizable()
-                                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                                        .padding(.top,20)
-                                        .padding(.bottom,-10)
-                                    Text("Task:\nDefine the big ideal")
-                                        .foregroundColor(.black)
-                                        .padding(.top)
-                                }
-                            })
+                ZStack{
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        NavigationLink(destination: FinalView()) {
+                            Image ("Ellipse 4")
+                                .resizable()
+                                .frame(width: 110, height: 110)
+                                .padding(.top,UIScreen.main.bounds.height/60)
+                                .padding(.bottom,UIScreen.main.bounds.width/60)
+                                .padding(.leading,UIScreen.main.bounds.width/60)
+                                .padding(.trailing,UIScreen.main.bounds.width/60)
                             
-                    }
-                    }.frame(width:UIScreen.main.bounds.width/2.2,height: UIScreen.main.bounds.height/4)
-                        .position(x:UIScreen.main.bounds.width/4,y:UIScreen.main.bounds.height/5)
-              
-                        
+                        }
+                    })
+                    NavigationLink(destination: FinalView()) {
+                        Text("Stop")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(.top,UIScreen.main.bounds.height/60)
+                            .padding(.bottom,UIScreen.main.bounds.width/60)
+                            .padding(.leading,UIScreen.main.bounds.width/60)
+                        .padding(.trailing,UIScreen.main.bounds.width/60)            }
+                }
+                
+                
+                HStack{
                     
-                    
-                    VStack{
-                        Image("avatar5")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
+                    VStack {
+                        VStack{
+                            Image("avatar1")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
                             
-                        Text ("Player3")
-                        ZStack{
-                            
+                            Text ("Player1")
                             ZStack{
                                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
                                     ZStack{
-                                        Image("Ellipse 25")
+                                        Image("Ellipse 23")
                                             .resizable()
                                             .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
                                             .padding(.top,20)
@@ -91,56 +64,87 @@ struct GameView_3: View {
                                     }
                                 })
                                 
-                        }
-                                
+                            }
+                        }.frame(width:UIScreen.main.bounds.width/2.2,height: UIScreen.main.bounds.height/4)
+                            .position(x:UIScreen.main.bounds.width/4,y:UIScreen.main.bounds.height/5)
+                        
+                        
+                        
+                        
+                        VStack{
+                            Image("avatar5")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
                             
-                        }
+                            Text ("Player3")
+                            ZStack{
+                                
+                                ZStack{
+                                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                                        ZStack{
+                                            Image("Ellipse 25")
+                                                .resizable()
+                                                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                                .padding(.top,20)
+                                                .padding(.bottom,-10)
+                                            Text("Task:\nDefine the big ideal")
+                                                .foregroundColor(.black)
+                                                .padding(.top)
+                                        }
+                                    })
+                                    
+                                }
+                                
+                                
+                            }
+                            
+                        }.frame(width:UIScreen.main.bounds.width/2.2,height: UIScreen.main.bounds.height/4)
+                        .position(x:UIScreen.main.bounds.width/2,y:UIScreen.main.bounds.height/4)}
+                    VStack {
+                        VStack{
+                            Image("avatar4")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                            
+                            Text ("Player2")
+                            ZStack{
+                                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                                    ZStack{
+                                        Image("Ellipse 24")
+                                            .resizable()
+                                            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                            .padding(.top,20)
+                                            .padding(.bottom,-10)
+                                        Text("Task:\nDefine the essential question")
+                                            .foregroundColor(.black)
+                                            .padding(.top)
+                                    }
+                                })
+                                
+                            }
+                        }.frame(width:UIScreen.main.bounds.width/2.2,height: UIScreen.main.bounds.height/4)
+                            .position(x:UIScreen.main.bounds.width/4,y:UIScreen.main.bounds.height/5);
+                        
+                        
+                    }
                     
-                    }.frame(width:UIScreen.main.bounds.width/2.2,height: UIScreen.main.bounds.height/4)
-                    .position(x:UIScreen.main.bounds.width/2,y:UIScreen.main.bounds.height/4)}
-                       VStack {
-                           VStack{
-                               Image("avatar4")
-                                   .resizable()
-                                   .aspectRatio(contentMode: .fit)
-                               
-                               Text ("Player2")
-                               ZStack{
-                                   Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                                       ZStack{
-                                           Image("Ellipse 24")
-                                               .resizable()
-                                               .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                                               .padding(.top,20)
-                                               .padding(.bottom,-10)
-                                           Text("Task:\nDefine the essential question")
-                                               .foregroundColor(.black)
-                                               .padding(.top)
-                                       }
-                                   })
-                                   
-                           }
-                           }.frame(width:UIScreen.main.bounds.width/2.2,height: UIScreen.main.bounds.height/4)
-                               .position(x:UIScreen.main.bounds.width/4,y:UIScreen.main.bounds.height/5);
-                           
-                     
+                    
+                    
                 }
                 
                 
+                
+                
+            }.background(Image("GameView3"))
+                .toolbar(.hidden)
             
-            }
-            
-           
             
             
-        }.background(Image("GameView3"))
-            .toolbar(.hidden)
-       
-            
-       
+        }
     }
 }
 
 #Preview {
     GameView_3()
 }
+
