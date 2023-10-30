d1e67dc93030d6de6a6e71d5333b3eb48df1963d
adrpr97 <130666088+adrpr97@users.noreply.github.com>
2023-10-30 10:56:34 +0100

    adriano
    smodealt

--- CollApp/View/SModeAlt.swift
+++ CollApp/View/SModeAlt.swift
@@ -19,6 +19,7 @@ presentationMode: Binding<PresentationMode>
     @State var nickname: String = ""
     @State var newTask: String = ""
     @State var secchio: String = ""
+    @State var selfoto: String = ""
     
     /*let images:[String] = ["noimage", "avatar1","avatar2","avatar3","avatar4","avatar5","avatar6","avatar7","avatar8","avatar9"]*/
     var fotine = ImageClass()
@@ -61,8 +62,7 @@ presentationMode: Binding<PresentationMode>
                         })
                     
                     VStack {
-                        ScrollView (.horizontal) {
-                            LazyHStack{
+                            TabView {
                                 ForEach(fotine.pupazzetti){pupazzetti in
                                     Image (pupazzetti.imageAvatar)
                                         .resizable()
@@ -71,12 +71,13 @@ presentationMode: Binding<PresentationMode>
                                         .clipped()
                                 }
                             }
-                            .scrollTargetLayout()
-                            
-                        }
-                        .mask(Rectangle())
-                        .scrollTargetBehavior(.viewAligned)
-                        .safeAreaPadding(.horizontal, 115)
+                            .frame(height: 130)
+                            .tabViewStyle(PageTabViewStyle())
+                            /*.mask(Rectangle())
+                            .scrollTargetBehavior(.viewAligned)
+                            .safeAreaPadding(.horizontal, 115)*/
+                        
+                        
                         
                         TextField("NickName", text: $nickname)
                             .padding([.leading,], 120.0)
@@ -91,7 +92,7 @@ presentationMode: Binding<PresentationMode>
                         
                         
                         
-                    } .padding(.top, 50.0)
+                    } .padding(.top, 100.0)
                     
                     
                     
